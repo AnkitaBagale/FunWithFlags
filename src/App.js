aa2ab89c070dabcf464e1d58890ba4d7267fbbc5
AnkitaBagale <69760792+AnkitaBagale@users.noreply.github.com>
2020-12-10 01:03:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const flagLib = {
+  "🇮🇳": "India",
+  "🇦🇫": "Afghanistan",
+  "🇦🇺": "Australia",
+  "🇧🇩": "Bangladesh",
+  "🇧🇷": "Brazil",
+  "🇧🇹": "Bhutan",
+  "🇨🇦": "Canada",
+  "🇨🇭": "Switzerland",
+  "🇩🇪": "Germany",
+  "🇪🇬": "Egypt",
+  "🇫🇷": "France",
+  "🇬🇧": "United Kingdom",
+  "🇮🇹": "Italy",
+  "🇱🇰": "Sri Lanka",
+  "🇺🇸": "United States",
+  "🇷🇺": "Russia",
+  "🇮🇱": "Israel",
+  "🇲🇾": "Malaysia"
+};
+
+const flags = Object.keys(flagLib);
+
+export default function App() {
+  var [country, setCountry] = useState("Country name will appear here..");
+
+  function flagInputHandler(event) {
+    var country = flagLib[event.target.value];
+    if (country === undefined) {
+      country = "This flag is not present in our database";
+    }
+    setCountry(country);
+  }
+
+  function flagClickHandler(flag) {
+    setCountry(flagLib[flag]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Fun With Flags</h1>
+      <h2>Enter flag or Choose any flag below to know country name</h2>
+      <input
+        onChange={flagInputHandler}
+        placeholder="Put flag here to know the country"
+      />
+      <div id="output" className="output">
+        {country}
+      </div>
+
+      <ul className="non-bullets">
+        {flags.map((flag) => {
+          return (
+            <li
+              onClick={() => flagClickHandler(flag)}
+              className="list-item"
+              key={flag}
+            >
+              <a href="#output">{flag}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
